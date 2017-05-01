16fa089c3cbbc73344491b07434fae5ac797737e
bbattyxx <wasunan_aum@hotmail.com>
2017-05-01 19:49:20 +0700

    Change blood type

--- bloodgroup.c
+++ bloodgroup.c
@@ -44,5 +44,5 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye bye bye\n");
+  printf("Goodbye bye\n");
 }
